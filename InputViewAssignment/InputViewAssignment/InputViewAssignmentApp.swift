1ab72f6cd13c8f129ed0a5d7ac8f62f185c7e279
m-mikula <mikulamartin27@gmail.com>
2025-04-09 10:21:52 +0200

    Initial commit plain project

---
+++ InputViewAssignment/InputViewAssignment/InputViewAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputViewAssignmentApp.swift
+//  InputViewAssignment
+//
+//  Created by Martin Mikula on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct InputViewAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
